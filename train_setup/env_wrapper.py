670a16f2bd27bb1749b51717d5f311786fc0773a
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2024-09-02 10:50:35 +0200

    Add critical environments

--- train_setup/env_wrapper.py
+++ train_setup/env_wrapper.py
@@ -6,8 +6,8 @@ class EnvWrapper(gym.Wrapper):
     def __init__(self, env, config):
         gym.Wrapper.__init__(self, env)
         self.name = config["ENVIRONMENT"]
-        if self.name == "Pendulum-v1":
-            env.action_space = spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)
+        if self.name in config["NORMAL_ENV"]:
+            env.action_space = spaces.Box(low=-1.0, high=1.0, shape=env.observation_space.shape, dtype=np.float32)
 
     def reset(self, seed: int):
         state = self.env.reset(seed=seed)[0]
@@ -15,8 +15,10 @@ class EnvWrapper(gym.Wrapper):
 
     def step(self, action):
         action = action.squeeze(0).numpy()
-        if self.env == "Pendulum-v1":
+        if self.name == "Pendulum-v1":
             action = action * 2.0
+        if self.name == "InvertedPendulum-v5":
+            action = action * 3.0
         next_state, reward, terminated, truncated, info = self.env.step(action)
         next_state = torch.from_numpy(next_state).unsqueeze_(dim=0).float()
         reward = torch.tensor(reward).view(1, -1)
